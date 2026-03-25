5f0d85d205363a5859848824d4c986b610fe4bc2
Rajendra kumar Mohanty <rajendra.mohanty@knorex.com>
2026-03-25 19:31:25 +0530

    to - do - app

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://tasks-app-backend-liart.vercel.app/api",
 });
 
 export default API;
